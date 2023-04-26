44c3d77c511820d5516ae559271fbdc7dadcb70d
hworld <hworld@hworld.hworld.com>
2023-04-25 21:00:32 -0300

    RUST ready

--- RUST/rust/src/chunk_pool.rs
+++ RUST/rust/src/chunk_pool.rs
@@ -1,34 +1,36 @@
 use std::thread;
 
 pub fn gauss(n_size: usize, b: &mut Vec<f64>, x: &mut Vec<f64>, a: &mut Vec<Vec<f64>>, num_thread: usize){
-	let mut threads_num = Vec::new();
+	let pool = rayon::ThreadPoolBuilder::new().num_threads(num_thread).build().unwrap();
 	
 	for norm in 0..(n_size-1) {
-		for i in 0..(num_thread){
-			let mut at = a.clone();
-			let mut bt = b.clone();
-			let t = thread::spawn(move ||{
-				for row in norm+1..n_size {
-					let multiplier = at[row][norm]/at[norm][norm];
-					for col in norm..n_size {
-						at[row][col] -= at[norm][col] * multiplier;
+		let at = a.clone();
+		let bt = b.clone();
+		pool.install(move ||{
+			let mut threads_num = Vec::new();
+			for _ in 0..(num_thread){
+				let mut att = at.clone();
+				let mut btt = bt.clone();
+				let t = thread::spawn(move ||{
+					for row in norm+1..n_size {
+						let multiplier = att[row][norm]/att[norm][norm];
+						for col in norm..n_size {
+							att[row][col] -= att[norm][col] * multiplier;
+						}
+						btt[row] -= btt[norm] * multiplier;
 					}
-					bt[row] -= bt[norm] * multiplier;
-				}
-				//print thread numbers t see if its working
-				//println!("thread = {}", norm);
-			});
-			//print number of chunks of threads
-			println!("chunk = {}", i);
-			threads_num.push(t);
-		}
-	
+					//print thread numbers t see if its working
+					//println!("thread = {}", norm);
+				});
+				//print number of chunks of threads
+				//println!("chunk = {}", i);
+				threads_num.push(t);
+			}
+			for t in threads_num {
+	 			t.join().expect("thread failed!");
+			}	
+		});
 	}
-	
-	for t in threads_num {
-		t.join().expect("thread failed!");
-	}
-	
 	
 	for row in  (0..(n_size)).rev() {
 		x[row] = b[row];
